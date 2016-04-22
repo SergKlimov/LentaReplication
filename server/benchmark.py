c965345bc3e2c2a355293d7f3b875d929195919b
Artyom Aleksyuk <artyom.h31@gmail.com>
2016-04-23 02:05:08 +0300

    Main agent

---
+++ server/benchmark.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import requests
+import time
+import random
+import sys
+import multiprocessing
+
+def push(process_number):
+    print("Process {} started".format(process_number))
+    time.sleep(random.random())
+
+    for x in range(iterations):
+        start = time.perf_counter()
+        num = process_number*process_count+x
+        # print("Process {} pushes {}".format(process_number, num))
+        payload = {"number": num,
+                   "status": "JSON" + str(num)}
+        r = requests.post("http://127.0.0.1:4567/push", json=payload)
+        finish = time.perf_counter()
+        time.sleep(0.1)
+        # time.sleep(1 - (finish - start))
+
+
+process_count = 30
+iterations = process_count
+
+for x in range(process_count):
+    p = multiprocessing.Process(target=push, args=(x,))
+    p.start()
---
+++ server/src/main/java/ru/spbstu/kspt/Main.java
@@ -0,0 +1,126 @@
+/**
+ * Created by artyom on 22.04.16.
+ */
+
+package ru.spbstu.kspt;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.sql2o.Connection;
+import org.sql2o.Query;
+import org.sql2o.Sql2o;
+import org.sql2o.data.Table;
+import org.sql2o.quirks.PostgresQuirks;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+public class Main {
+    private static void createTable(Sql2o sql2o) {
+        String sql =
+                "CREATE TABLE CHK (" +
+                        "    number INT PRIMARY KEY," +
+                        "    status VARCHAR(10) NOT NULL" +
+                        ")";
+
+        try (Connection con = sql2o.beginTransaction()) {
+            con.createQuery(sql).executeUpdate();
+            con.commit();
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        Timer timer = new Timer();
+        timer.schedule(new CountInserts(), 0, 1000);
+
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:postgresql://localhost/test");
+        config.setUsername("test_user");
+        config.setPassword("qwerty");
+//        config.addDataSourceProperty("cachePrepStmts", "true");
+//        config.addDataSourceProperty("prepStmtCacheSize", "250");
+//        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        HikariDataSource ds = new HikariDataSource(config);
+
+
+        Sql2o sql2o = new Sql2o(ds, new PostgresQuirks());
+//        sql2o.getDataSource().getConnection().setAutoCommit(false);
+        createTable(sql2o);
+        sql2o.open().createQuery("DELETE FROM CHK").executeUpdate();
+        Push push = new Push(sql2o);
+        post("/push", push);
+        get("/checkIndexes", (request, response) -> {
+            StringBuilder sb = new StringBuilder();
+            Table table = sql2o.open()
+                    .createQuery("SELECT * FROM CHK ORDER BY NUMBER")
+                    .executeAndFetchTable();
+            for (int i = 0; i < table.rows().size(); i++) {
+                Integer num = table.rows().get(i).getInteger("NUMBER");
+                if (num.equals(i)) {
+                    sb.append(String.format("OK! %d == %d\n", num, i));
+                } else {
+                    sb.append(String.format("Error! %d != %d\n", num, i));
+                    return sb.append("Error!\n").toString();
+                }
+            }
+            return sb.append("OK\n").toString();
+        });
+    }
+}
+
+class Push implements Route {
+    static AtomicInteger insertCount = new AtomicInteger();
+    Sql2o sql2o;
+
+    public Push(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+
+        Map<String, Object> map = mapper.readValue(request.bodyAsBytes(),
+                new TypeReference<Map<String, Object>>() {
+                });
+
+        String sql2 = "INSERT INTO CHK VALUES (:number, :status)";
+
+        try (Connection con = sql2o.beginTransaction()) {
+            Query query = con.createQuery(sql2);
+            for (Map.Entry<String, Object> entry : map.entrySet()) {
+                query.addParameter(entry.getKey(), entry.getValue());
+            }
+            query.executeUpdate();
+            con.commit();
+        }
+
+        insertCount.incrementAndGet();
+
+        return response;
+    }
+}
+
+class CountInserts extends TimerTask {
+    int oldValue = 0;
+
+    public void run() {
+        int newValue = Push.insertCount.get();
+        int diff = newValue - oldValue;
+        if (diff != 0) {
+            System.out.println("Inserts: " + diff);
+        }
+        oldValue = newValue;
+    }
+}
